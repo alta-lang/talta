259ac7cdfa3e94e27255d2b599325f9b822aad7a
Ariel Abreu <facekapow@outlook.com>
2019-05-12 08:21:02 -0400

    Make sure post- increment and decrement are marked as such (post); Update to alta-lang/alta-core@145fc7a54342ea065abcfe47cce48f079ee2450d; Update to alta-lang/ceetah@5a715f8bad91db63b7ae0c4c08d94efae6563bfc;

--- src/talta.cpp
+++ src/talta.cpp
@@ -1839,7 +1839,11 @@ std::shared_ptr<Ceetah::AST::Expression> Talta::CTranspiler::transpile(AltaCore:
     auto op = dynamic_cast<AAST::UnaryOperation*>(node);
     auto info = dynamic_cast<DH::UnaryOperation*>(_info);
 
-    return source.createUnaryOperation((CAST::UOperatorType)op->type, transpile(op->target.get(), info->target.get()));
+    return source.createUnaryOperation(
+      (CAST::UOperatorType)op->type,
+      transpile(op->target.get(), info->target.get()),
+      op->type == AAST::UOperatorType::PostIncrement || op->type == AAST::UOperatorType::PostDecrement
+    );
   } else if (nodeType == AltaNodeType::SizeofOperation) {
     auto op = dynamic_cast<AAST::SizeofOperation*>(node);
     auto info = dynamic_cast<DH::SizeofOperation*>(_info);
